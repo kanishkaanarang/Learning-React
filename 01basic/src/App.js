26eb95cf2e4d4e8a0a4bbbdd2f71abffccf8d5d8
Kanishka Narang <kanishkaanarang@gmail.com>
2025-06-26 14:00:11 +0000

    first

---
+++ 01basic/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>hi all</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+  <h1>hello ji kaise ho? | vite project , i am kanishka </h1>
+  )
+}
+
+export default App
